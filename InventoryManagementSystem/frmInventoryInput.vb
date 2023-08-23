0e012b1ff9935b55d15052e010d44005393aa1b1
tdc-takase <takase@tdc-ict.co.jp>
2023-08-23 19:59:03 +0900

    一通り機能完成

--- InventoryManagementSystem/frmInventoryInput.vb
+++ InventoryManagementSystem/frmInventoryInput.vb
@@ -1,4 +1,6 @@
-﻿Public Class frmInventoryInput
+﻿Imports System.Text
+
+Public Class frmInventoryInput
 
     ''' <summary>
     ''' 追加済データの登録／未登録
@@ -10,48 +12,169 @@
     ''' </summary>
     Private fpInventoryDataTable As DataTable
 
+    ''' <summary>
+    ''' 在庫データ接続クラス
+    ''' </summary>
+    Private fprInventoryAdapter As clsInventoryAdapter
+
 
     Private Sub frmInventoryInput_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        fpInventoryDataTable = New DataTable
+        Me.InputClear(True)
 
-        Me.dgvInventory.DataSource = fpInventoryDataTable
+        Me.fpInventoryDataTable = New DataTable
 
+        Me.fprInventoryAdapter = New clsInventoryAdapter
+        Me.fpInventoryDataTable = fprInventoryAdapter.FillInventoryLog()
 
-        Dim wNewDataRow As DataRow = fpInventoryDataTable.NewRow
+        Me.dgvInventory.DataSource = Me.fpInventoryDataTable
 
-        'wNewDataRow.co.Item("hoge") = 1
-
-        fpInventoryDataTable.ImportRow(wNewDataRow)
 
     End Sub
 
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
 
-        'Field(Of Integer)("hoge") = 1
+        Dim wNewRow As DataRow = fpInventoryDataTable.NewRow()
 
+        If (Me.CheckAddData() = False) Then
+            Me.txtTantou.Focus()
+            Return
+        End If
+
+        wNewRow.Item("SyoriKubun") = {"1", "2"}(Me.cmbInputType.SelectedIndex)
+        wNewRow.Item("Hinmei") = Me.txtHinmei.Text.ToString()
+        wNewRow.Item("Suuryou") = CInt(Me.numSuryou.Value)
+        wNewRow.Item("Tani") = Me.txtTani.Text
+        wNewRow.Item("Kingaku") = CInt(Me.numKingaku.Value)
+
+        wNewRow.Item("Tantousya") = Me.txtTantou.Text
+        wNewRow.Item("Bikou") = Me.txtBikou.Text
+        wNewRow.Item("SyoriDateTime") = Me.dtpInputDate.Value
+        wNewRow.Item("InputDateTime") = DateTime.MinValue
+        wNewRow.Item("UpdateDateTime") = DateTime.MinValue
+
+        fpInventoryDataTable.Rows.Add(wNewRow)
+
+        Me.SetingUnregisted()
+        Me.InputClear(False)
+        Me.txtHinmei.Focus()
     End Sub
 
     Private Sub btnRegist_Click(sender As Object, e As EventArgs) Handles btnRegist.Click
+        If (fprInventoryAdapter.InsertInventoryLog(fpInventoryDataTable) > 0) Then
+            Me.ReleaseUnregisted()
+
+        End If
 
     End Sub
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
-        Me.Close()
+        If (Me.fprIsUnregisted = True) Then
+            MessageBox.Show(Me,
+                            "未登録のデータがあります。終了してもよろしいですか？",
+                            Me.Text,
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Warning,
+                            MessageBoxDefaultButton.Button2)
+        Else
+            Me.Close()
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' 入力データのクリア
+    ''' </summary>
+    ''' <param name="aNewForm"></param>
+    Private Sub InputClear(ByVal aNewForm As Boolean)
+        If (aNewForm) Then
+            Me.txtTantou.Clear()
+            Me.ReleaseUnregisted()
+
+        End If
+
+        Me.cmbInputType.SelectedIndex = 0
+        Me.txtHinmei.Clear()
+        Me.numSuryou.Value = 0
+        Me.numKingaku.Value = 0
+        Me.txtTani.Clear()
+        Me.txtBikou.Clear()
 
     End Sub
 
 
-    Private Function fetchInventoryDataTable() As DataSet
-        Dim wNewDataSet As DataSet
+    ''' <summary>
+    ''' 追加データのチェック
+    ''' </summary>
+    ''' <returns></returns>
+    Private Function CheckAddData() As Boolean
+        Dim wSblWarningMessage As New StringBuilder
+        Dim wCheckSuccess As Boolean = True
 
+        If (Me.txtTantou.Text.Trim.Length = 0) Then
+            wSblWarningMessage.Append("・担当者が入力されていません" & vbCrLf)
+            wCheckSuccess = False
 
-        Dim wDataAdapter As Data.Common.DataAdapter
-        'Data.Common.DataAdapter.
+        End If
 
-        wDataAdapter.Fill(wNewDataSet)
+        If (Me.txtHinmei.Text.Trim.Length = 0) Then
+            wSblWarningMessage.Append("・品名が入力されていません" & vbCrLf)
+            wCheckSuccess = False
 
-        Return wNewDataSet
+        End If
+
+        If (Me.numSuryou.Value = 0) Then
+            wSblWarningMessage.Append("・数量が入力されていません" & vbCrLf)
+            wCheckSuccess = False
+
+        End If
+
+        If (Me.txtTani.Text.Trim.Length = 0) Then
+            wSblWarningMessage.Append("・単位が入力されていません" & vbCrLf)
+            wCheckSuccess = False
+
+        End If
+
+        If (wCheckSuccess = False) Then
+            MessageBox.Show(Me, wSblWarningMessage.ToString(),
+                            Me.Text,
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Warning,
+                            MessageBoxDefaultButton.Button1)
+        End If
+
+        Return wCheckSuccess
 
     End Function
+
+#Region "未登録状態の管理"
+    ''' <summary>
+    ''' 未登録フラグ
+    ''' </summary>
+    Private fprIsUnregisted As Boolean = False
+
+    ''' <summary>
+    ''' 未登録状態の設定
+    ''' </summary>
+    Private Sub SetingUnregisted()
+        If (Me.fprIsUnregisted = False) Then
+            Me.btnRegist.BackColor = Color.Red
+            Me.fprIsUnregisted = True
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' 未登録状態の解除
+    ''' </summary>
+    Private Sub ReleaseUnregisted()
+        Me.btnRegist.BackColor = SystemColors.Control
+        Me.fprIsUnregisted = False
+
+    End Sub
+
+#End Region
+
+
+
 
 End Class
--- InventoryManagementSystem/frmInventoryViewer.vb
+++ InventoryManagementSystem/frmInventoryViewer.vb
@@ -11,17 +11,69 @@
     Private fpInventoryDataTable As DataTable
 
 
+    Private fprInventoryAdapter As clsInventoryAdapter
+
+
+    Private Enum InvGridColumns
+        vSyoriKubun
+        vHinmei
+        vSuuryou
+        vTani
+        vKingaku
+        vTantousya
+        vBikou
+        vSyoriDateTime
+
+    End Enum
+
+
     Private Sub frmInventoryInput_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.InputClear()
+
+        Me.fpInventoryDataTable = New DataTable
+
+        Me.fprInventoryAdapter = New clsInventoryAdapter
+        Me.fpInventoryDataTable = fprInventoryAdapter.FillInventoryLog()
 
     End Sub
 
-    Private Sub btnAdd_Click(sender As Object, e As EventArgs)
-
-    End Sub
 
     Private Sub btnFind_Click(sender As Object, e As EventArgs) Handles btnFind.Click
+        Dim wViewDataRows As IEnumerable(Of DataRow) =
+            (From
+                 _dataRow In Me.fpInventoryDataTable
+             Where
+                 _dataRow.Field(Of String)("Tantousya").Contains(Me.txtTantou.Text.Trim) And
+                 _dataRow.Field(Of DateTime)("SyoriDateTime").Date = Me.dtpInputDate.Value.Date And
+                 _dataRow.Field(Of String)("SyoriKubun") = {"1", "2"}(Me.cmbInputType.SelectedIndex) And
+                 _dataRow.Field(Of String)("Hinmei").Contains(Me.txtHinmei.Text.Trim) And
+                 _dataRow.Field(Of String)("Bikou").Contains(Me.txtBikou.Text.Trim))
+
+        If (wViewDataRows.Any = False) Then
+            MessageBox.Show(Me, "件数がゼロ件です。", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1)
+            Me.txtTantou.Focus()
+            Return
+        End If
+
+        Dim wRowCount As Integer = wViewDataRows.Count
+        Me.dgvInventory.RowCount = wRowCount
+
+        For _dtIdx As Integer = 0 To wRowCount - 1
+            Dim wDataRow As DataRow = wViewDataRows(_dtIdx)
+            Me.dgvInventory.Item(InvGridColumns.vSyoriKubun, _dtIdx).Value =
+                If(wDataRow.Field(Of String)("SyoriKubun") = "1", "入庫", "出庫")
+            Me.dgvInventory.Item(InvGridColumns.vHinmei, _dtIdx).Value = wDataRow.Field(Of String)("Hinmei")
+            Me.dgvInventory.Item(InvGridColumns.vSuuryou, _dtIdx).Value = wDataRow.Field(Of Integer)("Suuryou")
+            Me.dgvInventory.Item(InvGridColumns.vTani, _dtIdx).Value = wDataRow.Field(Of String)("Tani")
+            Me.dgvInventory.Item(InvGridColumns.vKingaku, _dtIdx).Value = wDataRow.Field(Of Integer)("Kingaku")
+            Me.dgvInventory.Item(InvGridColumns.vTantousya, _dtIdx).Value = wDataRow.Field(Of String)("Tantousya")
+            Me.dgvInventory.Item(InvGridColumns.vBikou, _dtIdx).Value = wDataRow.Field(Of String)("Bikou")
+            Me.dgvInventory.Item(InvGridColumns.vSyoriDateTime, _dtIdx).Value = wDataRow.Field(Of DateTime)("SyoriDatetime").ToString("yy/MM/dd HH:mm")
+
+        Next
 
     End Sub
+
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Me.Close()
@@ -29,17 +81,14 @@
     End Sub
 
 
-    Private Function fetchInventoryDataTable() As DataSet
-        Dim wNewDataSet As DataSet
-
-
-        Dim wDataAdapter As Data.Common.DataAdapter
-        'Data.Common.DataAdapter.
-
-        wDataAdapter.Fill(wNewDataSet)
-
-        Return wNewDataSet
-
-    End Function
+    ''' <summary>
+    ''' 入力データのクリア
+    ''' </summary>
+    Private Sub InputClear()
+        Me.txtTantou.Clear()
+        Me.cmbInputType.SelectedIndex = 0
+        Me.txtHinmei.Clear()
+        Me.txtBikou.Clear()
+    End Sub
 
 End Class
